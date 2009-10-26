1ba5deb62e29595e37f4906f77a2759c58046a0e
Gao Guanhua <B22826@freescale.com>
2009-10-26 14:48:33 +0800

    Add AMP support for P2020DS
    Signed-off-by: Li Yang <leoli@freescale.com>

--- arch/powerpc/platforms/85xx/mpc85xx_ds.c
+++ arch/powerpc/platforms/85xx/mpc85xx_ds.c
@@ -79,7 +79,8 @@ void __init mpc85xx_ds_pic_init(void)
 		return;
 	}
 
-	if (of_flat_dt_is_compatible(root, "fsl,MPC8572DS-CAMP")) {
+	if (of_flat_dt_is_compatible(root, "fsl,MPC8572DS-CAMP") ||
+		of_flat_dt_is_compatible(root, "fsl,P2020DS-CAMP")) {
 		mpic = mpic_alloc(np, r.start,
 			MPIC_PRIMARY |
 			MPIC_BIG_ENDIAN | MPIC_BROKEN_FRR_NIRQS,
