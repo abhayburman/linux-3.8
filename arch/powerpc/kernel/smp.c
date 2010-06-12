4e0280c077d0ae4bea948ab4cfa1025d20b5d2f8
Dave Liu <daveliu@freescale.com>
2010-06-12 16:28:24 +0800

    powerpc/85xx: add the sleep/deep sleep support
    * sleep
      in sleep power management mode, the clock of e500 core
      is turned off, the clocks of most IP blocks are shut off,
      only the modules clocks which allows to wake up the
      processor are still running.
    
      type echo standby > /sys/power/state to enter sleep mode
    
    * deep sleep
      in deep sleep power management mode, including sleep mode
      feature as above, additionally, the power supply is removed
      to e500 core and L2 cache.
    
      type echo mem > /sys/power/state to enter deep sleep mode
    
    Signed-off-by: Dave Liu <daveliu@freescale.com>
    Signed-off-by: Li Yang <leoli@freescale.com>
    Signed-off-by: Jin Qing <b24347@freescale.com>

--- arch/powerpc/kernel/smp.c
+++ arch/powerpc/kernel/smp.c
@@ -319,6 +319,8 @@ int generic_cpu_disable(void)
 	set_cpu_online(cpu, false);
 #ifdef CONFIG_PPC64
 	vdso_data->processorCount--;
+#endif
+#if defined(CONFIG_PPC64) || defined(CONFIG_E500)
 	fixup_irqs(cpu_online_mask);
 #endif
 	return 0;
@@ -338,7 +340,7 @@ int generic_cpu_enable(unsigned int cpu)
 	while (!cpu_online(cpu))
 		cpu_relax();
 
-#ifdef CONFIG_PPC64
+#if defined(CONFIG_PPC64) || defined(CONFIG_E500)
 	fixup_irqs(cpu_online_mask);
 	/* counter the irq disable in fixup_irqs */
 	local_irq_enable();
