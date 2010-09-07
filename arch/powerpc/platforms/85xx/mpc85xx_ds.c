c80464f156d188d11eacc0d42ed5cb5db6ec72f8
Jin Qing <b24347@freescale.com>
2010-09-07 15:48:09 +0800

    Don't scan pci bridge when configure as endpoint
    Signed-off-by: Jin Qing <b24347@freescale.com>

--- arch/powerpc/platforms/85xx/mpc85xx_ds.c
+++ arch/powerpc/platforms/85xx/mpc85xx_ds.c
@@ -148,6 +148,54 @@ static int mpc85xx_exclude_device(struct pci_controller *hose,
 }
 #endif	/* CONFIG_PCI */
 
+#ifdef CONFIG_PCI
+/*Host/agent status can be read from porbmsr in the global utilities*/
+static int get_mpc85xx_ds_host_agent(void)
+{
+	struct device_node *np;
+	void __iomem *gur_regs;
+	u32 host_agent;
+
+	np = of_find_compatible_node(NULL, NULL, "fsl,mpc8548-guts");
+	if (np == NULL) {
+		printk(KERN_ERR "Could not find global-utilities node\n");
+		return 0;
+	}
+
+	gur_regs = of_iomap(np, 0);
+	of_node_put(np);
+	if (!gur_regs) {
+		printk(KERN_ERR "Failed to map global-utilities register space\n");
+		return 0;
+	}
+	host_agent = (in_be32(gur_regs + 4) & 0x00070000) >> 16 ;
+
+	iounmap(gur_regs);
+
+	return host_agent;
+}
+
+/*
+ * To judge if the PCI(e) controller is host/agent mode through
+ * the PORBMSR register.
+ *     0: agent mode
+ *     1: host mode
+ */
+static bool mpc85xx_ds_pci_is_host(u32 host_agent, resource_size_t res)
+{
+	switch (res & 0xfffff) {
+	case 0xa000:    /* PCIe1 */
+		return host_agent & 0x2;
+		break;
+	case 0x9000:    /* PCIe2 */
+		return host_agent & 0x1;
+		break;
+	default:
+		return true;
+	}
+}
+#endif
+
 /*
  * Setup the architecture
  */
@@ -159,6 +207,7 @@ static void __init mpc85xx_ds_setup_arch(void)
 #ifdef CONFIG_PCI
 	struct device_node *np;
 	struct pci_controller *hose;
+	u32 host_agent;
 #endif
 	dma_addr_t max = 0xffffffff;
 
@@ -166,12 +215,15 @@ static void __init mpc85xx_ds_setup_arch(void)
 		ppc_md.progress("mpc85xx_ds_setup_arch()", 0);
 
 #ifdef CONFIG_PCI
+	host_agent = get_mpc85xx_ds_host_agent();
 	for_each_node_by_type(np, "pci") {
 		if (of_device_is_compatible(np, "fsl,mpc8540-pci") ||
 		    of_device_is_compatible(np, "fsl,mpc8548-pcie") ||
 		    of_device_is_compatible(np, "fsl,p2020-pcie")) {
 			struct resource rsrc;
 			of_address_to_resource(np, 0, &rsrc);
+			if (!mpc85xx_ds_pci_is_host(host_agent, rsrc.start))
+				continue;
 			if ((rsrc.start & 0xfffff) == primary_phb_addr)
 				fsl_add_bridge(np, 1);
 			else
