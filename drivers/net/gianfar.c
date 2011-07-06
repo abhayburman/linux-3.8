f7a76b3b0d3563a9f8a6e3d0b1ee1a44fcfe69ac
Akhil Goyal <akhil.goyal@freescale.com>
2011-07-06 22:25:15 +0000

    eTSEC: Bug fix in hash programming for packet steering
    In software packet steering hash is calculated on the basis of
    IPPROTO_ESP for ipsec flows. For this security parameter index
    should be obtained from the packet header and in order to get
    security parameter index from the packet
     len >= ipheader + esp header
    
    Signed-off-by: Akhil Goyal <akhil.goyal@freescale.com>
    Signed-off-by: Li Yang <leoli@freescale.com>

--- drivers/net/gianfar.c
+++ drivers/net/gianfar.c
@@ -1484,11 +1484,11 @@ static int get_cpu_number(unsigned char *eth_pkt, int len)
 	case IPPROTO_ESP:
 	case IPPROTO_SCTP:
 	case IPPROTO_UDPLITE:
-		if (len < (ETH_HLEN + (ihl * 4) + 4))
+		if (len >= (ETH_HLEN + (ihl * 4) + 4))
 			ports = *((u32 *) (eth_pkt + ETH_HLEN + (ihl * 4)));
 		break;
 	case IPPROTO_AH:
-		if (len < (ETH_HLEN + (ihl * 4) + 4))
+		if (len >= (ETH_HLEN + (ihl * 4) + 4))
 			ports = *((u32 *) (eth_pkt + ETH_HLEN + (ihl * 4) + 4));
 		break;
 
