nxp-qoriq-yocto-sdk/linux

KVM: PPC: bookehv: Allow PMRs to guest only when it is reserved for guest
By default,  guest access to PMRs (performance monitor registers) traps
into KVM and if the performance monitor facility is reserved for guest
then access to PMRs does not trap in KVM (accessed by guest without
trap).  This is controlled by MSRP.PMMP.

MSRP.PMMP is set or cleared in KVM_CREATE_VCPU ioctl handling on the
basis of whether performance monitor is reserved or not.  So it is
wrongly assumed that KVM_RESERVE_PERFMON ioctl will come before
KVM_CREATE_VCPU while KVM_RESERVE_PERFMON ioctl will always come after
KVM_CREATE_VCPU ioctl.

Now MSRP.PMMP is cleared in KVM_RESERVE_PERFMON ioctl handling.

Signed-off-by: Bharat Bhushan <bharat.bhushan@freescale.com>
Signed-off-by: Scott Wood <scottwood@freescale.com>

arch/powerpc/kvm/e500mc.c
@@ -173,11 +173,7 @@ int kvmppc_core_vcpu_setup(struct kvm_vcpu *vcpu)
 
 	vcpu->arch.epcr = EPCR_DSIGS | EPCR_DGTMI | EPCR_DUVD;
 	vcpu->arch.gpir = 0;
-
-	if (vcpu->arch.pm_is_reserved)
-		vcpu->arch.msrp = MSRP_UCLEP | MSRP_DEP;
-	else
-		vcpu->arch.msrp = MSRP_UCLEP | MSRP_DEP | MSRP_PMMP;
+	vcpu->arch.msrp = MSRP_UCLEP | MSRP_DEP | MSRP_PMMP;
 
 	/* Registers init */
 	vcpu->arch.pvr = mfspr(SPRN_PVR);

abhayburman/linux-3.8

arch/powerpc/kvm/e500mc.c
@@ -192,13 +192,9 @@ int kvmppc_core_vcpu_setup(struct kvm_vcpu *vcpu)
 
 	vcpu->arch.shadow_epcr = SPRN_EPCR_DSIGS | SPRN_EPCR_DGTMI | \
 				 SPRN_EPCR_DUVD;
+	vcpu->arch.shadow_msrp = MSRP_UCLEP | MSRP_DEP | MSRP_PMMP;
 	vcpu->arch.eplc = EPC_EGS | (vcpu->kvm->arch.lpid << EPC_ELPID_SHIFT);
 	vcpu->arch.epsc = vcpu->arch.eplc;
-
-	if (vcpu->arch.pm_is_reserved)
-		vcpu->arch.shadow_msrp = MSRP_UCLEP | MSRP_DEP;
-	else
-		vcpu->arch.shadow_msrp = MSRP_UCLEP | MSRP_DEP | MSRP_PMMP;
 
 	vcpu->arch.pvr = mfspr(SPRN_PVR);
 	vcpu_e500->svr = mfspr(SPRN_SVR);