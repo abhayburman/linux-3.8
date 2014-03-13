0d0575e647c95c988148a107839a1c1f31baa420
Cristian Sovaiala <cristian.sovaiala@freescale.com>
2014-03-13 11:45:55 +0200

    dpaa_eth: Add support for Wake on Lan
    Currently the system wakes up from sleep mode on Magic Packets.
    The ports that are configured as Auto Response will not have the
    WoL feature enabled. Magic Packets are not yet supported on
    those ports.
    
    Signed-off-by: Cristian Sovaiala <cristian.sovaiala@freescale.com>
    Change-Id: I579494dab0a0e918a2644483129951d370cbccf9
    Reviewed-on: http://git.am.freescale.net:8181/9904
    Tested-by: Review Code-CDREVIEW <CDREVIEW@freescale.com>
    Reviewed-by: Jose Rivera <German.Rivera@freescale.com>

--- drivers/net/ethernet/freescale/dpa/mac-api.c
+++ drivers/net/ethernet/freescale/dpa/mac-api.c
@@ -611,6 +611,7 @@ static void __cold setup_dtsec(struct mac_device *mac_dev)
 	mac_dev->fm_rtc_set_drift	= fm_rtc_set_drift;
 	mac_dev->fm_rtc_set_alarm	= fm_rtc_set_alarm;
 	mac_dev->fm_rtc_set_fiper	= fm_rtc_set_fiper;
+	mac_dev->set_wol		= fm_mac_set_wol;
 	mac_dev->dump_mac_regs		= dtsec_dump_regs;
 }
 
@@ -627,6 +628,7 @@ static void __cold setup_xgmac(struct mac_device *mac_dev)
 	mac_dev->get_mac_handle	= get_mac_handle;
 	mac_dev->set_tx_pause	= fm_mac_set_tx_pause_frames;
 	mac_dev->set_rx_pause	= fm_mac_set_rx_pause_frames;
+	mac_dev->set_wol	= fm_mac_set_wol;
 	mac_dev->dump_mac_regs	= xgmac_dump_regs;
 }
 
@@ -651,6 +653,7 @@ static void __cold setup_memac(struct mac_device *mac_dev)
 	mac_dev->fm_rtc_set_drift	= fm_rtc_set_drift;
 	mac_dev->fm_rtc_set_alarm	= fm_rtc_set_alarm;
 	mac_dev->fm_rtc_set_fiper	= fm_rtc_set_fiper;
+	mac_dev->set_wol		= fm_mac_set_wol;
 	mac_dev->dump_mac_regs		= memac_dump_regs;
 }
 
